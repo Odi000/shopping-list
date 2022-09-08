53c38d1b00652b9a656ec162344f9a4b059f1f1e
Odi000 <mandimandi.ro@gmail.com>
2022-09-08 18:22:56 +0200

    Separated the javascript and CSS in different files just to se the precentage in GitHub

---
+++ script.js
@@ -0,0 +1,38 @@
+const insert = document.querySelector('input')
+,uList = document.querySelector('ul')
+,mainButton = document.querySelector('button');
+
+//Declaring the function
+function addItem() {
+if (Boolean(insert.value)){
+
+const itemName = insert.value;
+insert.value = '';
+
+const item = document.createElement('li'),
+     span = document.createElement('span'),
+     btn = document.createElement('button');
+
+item.appendChild(span);
+item.appendChild(btn);
+
+span.textContent = itemName;
+btn.textContent = 'Delete';
+btn.addEventListener('click', () => {
+ uList.removeChild(item)
+});
+
+uList.appendChild(item);
+insert.focus();
+}
+}
+
+//The function when you Click the 'Add item' button
+mainButton.addEventListener('click', addItem);
+
+//If eneter is pressed the above function will run
+function enter(){
+if(event.key === 'Enter') {
+addItem();
+}
+}
